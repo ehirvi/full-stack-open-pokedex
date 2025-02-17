fa59549128db04f33baaf9e1feef0b2055d209ff
ehirvi <eino.hirvi@gmail.com>
2025-02-17 14:29:30 +0200

    Add E2E test for individual pokemon

--- e2e-tests/pokedex.spec.js
+++ e2e-tests/pokedex.spec.js
@@ -1,9 +1,18 @@
-const { test, describe, expect } = require('@playwright/test')
+const { test, describe, expect, beforeEach } = require('@playwright/test')
 
 describe('Pokedex', () => {
-  test('front page can be opened', async ({ page }) => {
+  beforeEach(async ({ page }) => {
     await page.goto('')
+  })
+
+  test('front page can be opened', async ({ page }) => {
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
+  })
+
+  test('individual pokemon page can be navigated to', async ({page}) => {
+    const ivysaur = page.getByRole('link', { name: 'ivysaur' })
+    await ivysaur.click()
+    await expect(page.getByText('chlorophyll')).toBeVisible()
   })
 })
