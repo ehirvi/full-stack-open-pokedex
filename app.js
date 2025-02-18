9e41d6f082815ac6bac8228b1d8eec854ebf4ada
ehirvi <eino.hirvi@gmail.com>
2025-02-18 14:10:41 +0200

    Add healthcheck for deployment and purposefully break it to test it

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const { error } = require('console')
 const express = require('express')
 const app = express()
 
@@ -5,6 +6,14 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
+
+app.get('/health', (req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  if (true) {
+    throw('error... ')
+  }
+  res.send('ok')
+})
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
